addfd8c2a715a914580cfd042406c677c1831184
Jimmy Pocock <jimmypocock@yahoo.com>
2015-08-23 18:51:27 -0500

    Cleaning up the styles and scripts.

---
+++ app/scripts/full-page.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  var resetDimensions = function() {
+    var winHeight = $(window).height(),
+        winWidth  = $(window).width();
+    $('#background').css('height', winHeight).css('width', winWidth);
+  }
+
+  resetDimensions();
+  $(window).on('resize', resetDimensions);
+
+});
